4094baefb125a03cc51bf27bcc5ebd5f642523fb
Helen Pribysh <helenpribysh@gmail.com>
2019-05-29 12:54:58 +0300

    Implemented Tas1

---
+++ Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Task1.Program;
+
+namespace Task1
+{
+    class Program
+    {
+        public delegate void Delegat(); 
+
+        static void Main(string[] args)
+        {
+            ConsoleShow consoleShow = new ConsoleShow();
+            consoleShow.TwoNumbers();
+
+            Console.Read();
+        }
+    }
+
+    class ConsoleShow
+    {
+        public void Show(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void TwoNumbers()
+        {
+            Sum sum = new Sum();
+            sum.Plus(4, 5, Show);
+        }
+    }
+
+    class Sum
+    {
+        public void Plus(int x, int y, Action<string> show)
+        {
+            int result = x + y;
+            show(result.ToString());
+        }
+    }
+}
+
